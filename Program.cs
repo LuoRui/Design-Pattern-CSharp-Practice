216c6f69191c598a88cf8fc9bced47a0d1731af0
LuoRui <>
2026-03-12 15:57:04 +0800

    添加模式名注释

--- Program.cs
+++ Program.cs
@@ -43,26 +43,44 @@ namespace DesignPattern
             ChainOfResponsibilityPattern.ChainOfResponsibilityPattern.Practice();
             // 命令模式
             CommandPattern.CommandPattern.Practice();
+            // 解释器模式
             InterpreterPattern.InterpreterPattern.Practice();
+            // 迭代器模式
             IteratorPattern.IteratorPattern.Practice();
+            // 中介者模式
             MediatorPattern.MediatorPattern.Practice();
+            // 备忘录模式
             MementoPattern.MementoPattern.Practice();
+            // 观察者模式
             ObserverPattern.ObserverPattern.Practice();
+            // 状态模式
             StatePattern.StatePattern.Practice();
+            // 空对象模式
             NullObjectPattern.NullObjectPattern.Practice();
+            // 策略模式
             StrategyPattern.StrategyPattern.Practice();
+            // 模板模式
             TemplatePattern.TemplatePattern.Practice();
+            // 访问者模式
             VisitorPattern.VisitorPattern.Practice();
             #endregion
 
             #region J2EE模式实践（C#实现）
+            // MVC模式
             MVCPattern.MVCPattern.Practice();
+            // 业务代表模式
             BusinessDelegatePattern.BusinessDelegatePattern.Practice();
+            // 组合实体模式
             CompositeEntityPattern.CompositeEntityPattern.Practice();
+            // 数据访问对象模式
             DataAccessObjectPattern.DataAccessObjectPattern.Practice();
+            // 前端控制器模式
             FrontControllerPattern.FrontControllerPattern.Practice();
+            // 拦截过滤器模式
             InterceptingFilterPattern.InterceptingFilterPattern.Practice();
+            // 服务定位器模式
             ServiceLocatorPattern.ServiceLocatorPattern.Practice();
+            // 传输对象模式
             TransferObjectPattern.TransferObjectPattern.Practice();
             #endregion
         }
